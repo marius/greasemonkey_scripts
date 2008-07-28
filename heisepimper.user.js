4fd09db463dd26dfb6bf9b6e719590ac98716f2e
Marius Nuennerich <marius@nuenneri.ch>
2008-07-29 00:21:52 +0200

    add license

--- heisepimper.user.js
+++ heisepimper.user.js
@@ -6,6 +6,15 @@
 // @include        http://*.heise.de/newsticker/*
 // @include        http://*.heise.de/tp/*
 // ==/UserScript==
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <marius@nuenneri.ch> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Marius Nuennerich
+ * ----------------------------------------------------------------------------
+ */
+
 
 (function() {
 	function pimp_news() {
