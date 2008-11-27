eabf2eb789d2e6b47038e83b81fb7815bddc7b86
Marius Nuennerich <marius@nuenneri.ch>
2008-11-27 12:51:42 +0100

    add stuff for heise open articles

--- heisepimper.user.js
+++ heisepimper.user.js
@@ -5,6 +5,7 @@
 // @namespace      http://github.com/Marius/greasemonky_scripts
 // @include        http://*.heise.de/newsticker/*
 // @include        http://*.heise.de/tp/*
+// @include        http://*.heise.de/open/*
 // ==/UserScript==
 /*
  * ----------------------------------------------------------------------------
@@ -18,6 +19,7 @@
 
 (function() {
 	function pimp_news() {
+		GM_log("news");
 		var date = document.title.substring(15, 23);
 		var shortTitle = document.title.replace(/heise online - \d\d\.\d\d\.\d\d - /, '');
 		var divs = document.getElementsByTagName('div');
@@ -34,14 +36,42 @@
 	}
 
 	function pimp_headings() {
+		GM_log("headings");
 		document.body.innerHTML = document.getElementById('mitte_news').innerHTML;
 	}
 
 	function pimp_telepolis() {
+		GM_log("tp");
 		var tables = document.getElementsByTagName('table');
 		for(var i = 0; i < tables.length; i++) {
 			if(tables[i].className == 'inhalt-table') {
 				document.body.innerHTML = '<table>' + tables[i].innerHTML + '</table>';
+				return;
+			}
+		}
+	}
+
+	function pimp_open_headings() {
+		GM_log("open headings");
+	}
+
+	function pimp_open() {
+		GM_log("open");
+		var date = document.title.substring(13, 21);
+		var shortTitle = document.title.replace(/heise open - \d\d\.\d\d\.\d\d - /, '');
+		var ps = document.getElementsByTagName('p');
+		var navi = '';
+		for(var i = 0; i < ps.length; i++) {
+			if(ps[i].className == 'artikel_navi') {
+				navi = ps[i].innerHTML;
+				break;
+			}
+		}
+		var divs = document.getElementsByTagName('div');
+		for(var i = 0; i < divs.length; i++) {
+			if(divs[i].className == 'meldung_wrapper') {
+				document.body.innerHTML = date + divs[i].innerHTML + navi;
+				document.title = shortTitle;
 				return;
 			}
 		}
@@ -60,9 +90,12 @@
 
 	if(document.title.match(/7-Tage-News/))
 		pimp_headings();
-	else if(document.title.match(/(TELEPOLIS|TP: )/))
+	else if(document.title.match(/^TELEPOLIS/))
 		pimp_telepolis();
+	else if(document.title.match(/Open Source/))
+		pimp_open_headings();
+	else if(document.title.match(/heise open/))
+		pimp_open();
 	else
 		pimp_news();
-
 }());
